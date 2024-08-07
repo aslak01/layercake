336f839523f847d7f35f09f37894b75e8ffba774
aslak <aslakrb@gmail.com>
2024-08-07 23:30:48 +0200

    fix: update typescript version, add missing package

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
 	...svelte.configs['flat/recommended'],
